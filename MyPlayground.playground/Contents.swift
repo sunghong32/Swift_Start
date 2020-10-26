f6bc3a274d22e088f4c4a7e71ddf53b430648c4b
민성홍 <sunghong32@naver.com>
2020-10-27 01:51:20 +0900

    introduce

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,11 @@
+let name: String = "sunghong"
+var age: Int = 100
+var job = "Programmar"
+var height = 180
+age = 26
+job = "iOS Programmer"
+var hobby = "soccer"
+var team = "Tottenham Hotspur"
+print("저의 이름은 \(name)이고, 나이는 \(age)세이며, 직업은 \(job)입니다. 키는 \(height)cm입니다.")
+print("취미는 \(hobby), 좋아하는 축구팀은 \(team)입니다.")
+print("Come on You Spurs!")
