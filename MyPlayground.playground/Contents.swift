79097b69b1c08038348e99f4ca72726f917b47ca
민성홍 <sunghong32@naver.com>
2021-01-03 16:23:50 +0900

    happy new year

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,11 +1,12 @@
 let name: String = "sunghong"
 var age: Int = 100
 var job = "Programmar"
-var height = 180
-age = 26
+age = 27
 job = "iOS Programmer"
 var hobby = "soccer"
 var team = "Tottenham Hotspur"
-print("저의 이름은 \(name)이고, 나이는 \(age)세이며, 직업은 \(job)입니다. 키는 \(height)cm입니다.")
+print("저의 이름은 \(name)이고, 나이는 \(age)세이며, 직업은 \(job)입니다.")
 print("취미는 \(hobby), 좋아하는 축구팀은 \(team)입니다.")
 print("Come on You Spurs!")
+print("Happy New Year!")
+
