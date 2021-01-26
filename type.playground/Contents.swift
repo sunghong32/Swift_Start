4da64b69eea765398bc551b48780b7cdaa2a717d
민성홍 <sunghong32@naver.com>
2021-01-26 12:45:46 +0900

    type

---
+++ type.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+let numberOne:Int = 123
+let numberTwo:Double = 123
+
+let numberThree:UInt = 123
+//let numberThree:UInt = -123
+//양의 정수만 넣을 수 있음.
+
+let character:Character = "A"
+//let character:Character = 'A'
+//"A" 나 '"A"'로 써야함.
+
+let string:String = "A"
+
+print(numberOne,numberTwo,numberThree,character,string)
+
