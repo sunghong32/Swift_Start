be5491516e53ef43a35444b4e2fd79048fa2f356
민성홍 <sunghong32@naver.com>
2021-01-26 12:44:40 +0900

    closure

---
+++ closure.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+func plus(a: Int,b: Int) -> Int{
+    return a+b
+}
+
+//let add: (Int,Int) -> Int = {$0 + $1}
+//let add: (Int,Int) -> Int = plus
+//let add: (Int,Int) -> Int = {return a + b}
+//let add: (Int,Int) -> Int = {(a,b) in return a + b}
+//let add: (Int,Int) -> Int = plus(a:b:)
+
+
+
+
